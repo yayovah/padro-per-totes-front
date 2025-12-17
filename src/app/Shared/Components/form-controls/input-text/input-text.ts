4b29094f9a3075a00e14bf9948c5084d4a52e1a0
yayovah <yayovah@gmail.com>
2025-12-17 11:11:31 +0100

    input-text shared component

--- src/app/Shared/Components/form-controls/input-text/input-text.ts
+++ src/app/Shared/Components/form-controls/input-text/input-text.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input-text',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './input-text.html',
   styleUrl: './input-text.scss',
 })
 export class InputText {
-
+  @Input() label = "";
+  @Input() type = "";
+  @Input() control! : FormControl;
+  @Input() placeholder = "";
+  @Input() errorMessage = "";
+  @Input() name = "";
+  @Input() required = "required";
 }
