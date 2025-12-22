2577a3d339c38d5c46536e6543c761b5f1b534dc
yayovah <yayovah@gmail.com>
2025-12-22 09:56:18 +0100

    preguntes & resposta Services implemented

--- src/app/Preguntes/Services/pregunta.ts
+++ src/app/Preguntes/Services/pregunta.ts
@@ -1,16 +1,61 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { inject, Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
 import { PreguntaDTO } from '../Models/pregunta.dto';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Pregunta {
-  getPreguntesByCiutat(ciutatId: number): Observable<PreguntaDTO[]>{}
 
-  createPregunta(dadesPregunta: Omit<PreguntaDTO, 'id'>, ciutatId: number): Observable<PreguntaDTO[]>{}
+  private readonly baseUrl = environment.apiUrl;
+  private readonly ciutatsEndpoint = '/preguntes';
+  private readonly url = `${this.baseUrl}${this.ciutatsEndpoint}`;
+  private http = inject( HttpClient );
 
-  updatePregunta(pregunta: PreguntaDTO): Observable<PreguntaDTO[]>{}
+  getPreguntesByCiutat(ciutatId: number): Observable<PreguntaDTO[]>{
+        //Petició a la api
+        return this.http
+        .get<PreguntaDTO[]>(`${this.url}/ciutat/${ciutatId}`)
+        .pipe(
+          //en cas d'error en la petició
+          catchError((error) => {
+            console.error('Error recuperando preguntas del servidor:', error);
+            return throwError(() => new Error('Error cargando preguntas'));
+          })
+        );
+  }
 
-  deletePregunta(preguntaId: number): Observable<any>{}
+  createPregunta(dadesPregunta: Omit<PreguntaDTO, 'id'>, ciutatId: number): Observable<PreguntaDTO>{
+    return this.http
+      .post<PreguntaDTO>(`${this.url}/ciutat/${ciutatId}`, dadesPregunta)
+      .pipe(
+          catchError((error) => {
+            console.error(`Error intentando crear la pregunta:`, error);
+            return throwError(() => new Error('Error creando pregunta'));
+        }
+      ));
+  }
+
+  updatePregunta(pregunta: PreguntaDTO): Observable<PreguntaDTO>{
+    return this.http
+      .put<PreguntaDTO>(`${this.url}/${pregunta.id}`, pregunta)
+        .pipe(catchError((error) => {
+          console.error(`Error actualizando la pregunta en el servidor:`, error);
+          return throwError(() => new Error('Error actualizando pregunta'));
+        }
+      ));
+  }
+
+  deletePregunta(preguntaId: number): Observable<any>{
+    return this.http
+      .delete<number>(`${this.url}/${preguntaId}`)
+      .pipe(
+          catchError((error) => {
+            console.error(`Error intentando eliminar la pregunta:`, error);
+            return throwError(() => new Error('Error eliminando pregunta'));
+        }        
+      ));
+  }
 }
--- src/app/Respostes/Services/resposta.ts
+++ src/app/Respostes/Services/resposta.ts
@@ -1,16 +1,62 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 import { RespostaDTO } from '../Models/resposta.dto';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Resposta {
-    getRespostesByPregunta(preguntaId: number): Observable<RespostaDTO[]>{}
+
+  private readonly baseUrl = environment.apiUrl;
+  private readonly ciutatsEndpoint = '/respostes';
+  private readonly url = `${this.baseUrl}${this.ciutatsEndpoint}`;
+  private http = inject( HttpClient );
+
+    getRespostesByPregunta(preguntaId: number): Observable<RespostaDTO[]>{
+      //Petició a la api
+      return this.http
+      .get<RespostaDTO[]>(`${this.url}/pregunta/${preguntaId}`)
+      .pipe(
+        //en cas d'error en la petició
+        catchError((error) => {
+          console.error('Error recuperando respuestas del servidor:', error);
+          return throwError(() => new Error('Error cargando respuestas'));
+        })
+      );
+    }
   
-    createResposta(dadesResposta: Omit<RespostaDTO, 'id'>, preguntaId: number): Observable<RespostaDTO[]>{}
+    createResposta(dadesResposta: Omit<RespostaDTO, 'id'>, preguntaId: number): Observable<RespostaDTO>{
+      return this.http
+        .post<RespostaDTO>(`${this.url}/pregunta/${preguntaId}`, dadesResposta)
+        .pipe(
+            catchError((error) => {
+              console.error(`Error intentando crear la respuesta:`, error);
+              return throwError(() => new Error('Error creando respuesta'));
+          }
+        ));
+    }
   
-    updateResposta(resposta: RespostaDTO): Observable<RespostaDTO[]>{}
+    updateResposta(resposta: RespostaDTO): Observable<RespostaDTO>{
+    return this.http
+      .put<RespostaDTO>(`${this.url}/${resposta.id}`, resposta)
+        .pipe(catchError((error) => {
+          console.error(`Error actualizando la respuesta en el servidor:`, error);
+          return throwError(() => new Error('Error actualizando respuesta'));
+        }
+      ));
+
+    }
   
-    deleteResposta(respostaId: number): Observable<any>{}
+    deleteResposta(respostaId: number): Observable<any>{
+      return this.http
+        .delete<number>(`${this.url}/${respostaId}`)
+        .pipe(
+            catchError((error) => {
+              console.error(`Error intentando eliminar la respuesta:`, error);
+              return throwError(() => new Error('Error eliminando respuesta'));
+          }        
+        ));
+    }
 }
