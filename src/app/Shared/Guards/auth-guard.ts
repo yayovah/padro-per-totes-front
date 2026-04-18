2ce0376599ee7815be98f592d1bde1e7ee73fb5c
Xavier Vallverdú i Poblet <xvallverdup@uoc.edu>
2026-04-18 16:08:37 +0200

    guard actualitzation

--- src/app/Shared/Guards/auth-guard.ts
+++ src/app/Shared/Guards/auth-guard.ts
@@ -2,15 +2,24 @@ import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Router } from '@angular/router';
-import { selectAuthRol } from '../../Auth/Selectors/auth.selector';
-import { map, tap } from 'rxjs';
+//import { selectAuthRol } from '../../Auth/Selectors/auth.selector';
+//import { map, tap } from 'rxjs';
 //import { AuthSelectors } from '../../Auth/Store/Selectors/auth.selectors';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  const store = inject(Store);
+ const store = inject(Store);
+  const storage = inject(Storage);
   const rolNecessari = route.data['rolNecessari'];
 
-  return store.select(selectAuthRol).pipe(
+  if (!storage.getItem('userRol') || storage.getItem('userRol') === rolNecessari) {
+    const router = inject(Router);
+    router.navigate(['/login']);
+    return false;
+  }
+  return true;
+
+/*
+  return store.select('selectAuthRol').pipe(
     map((rol) => {
       console.log("usuari rol: ", );
       if (rol !== rolNecessari ) {
@@ -21,5 +30,5 @@ export const authGuard: CanActivateFn = (route, state) => {
       return true;
       }
     )
-  )
+  )*/
 };
