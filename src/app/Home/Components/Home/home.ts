fe27f81c465e044380f9981c8142aa7ff97b8522
Xavier Vallverdú i Poblet <xvallverdup@uoc.edu>
2026-04-20 18:43:37 +0200

    Home d'usuari

--- src/app/Home/Components/Home/home.ts
+++ src/app/Home/Components/Home/home.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { CiutatDTO } from '../../../Ciutat/Models/ciutat.dto';
+import { Ciutats } from '../../../Ciutat/Services/ciutats';
+import { RespostaDTO } from '../../../Respostes/Models/resposta.dto';
+import { PreguntaDTO } from '../../../Preguntes/Models/pregunta.dto';
+import { SituacioDTO } from '../../../Situacions/Model/situacio.dto';
+import { Pregunta } from '../../../Preguntes/Services/pregunta';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +13,47 @@ import { Component } from '@angular/core';
   styleUrl: './home.scss',
 })
 export class Home {
+  private ciutatsService = inject(Ciutats);
+  ciutats = signal<CiutatDTO[]>([]);
+  ciutatSeleccionada = signal<CiutatDTO | null>(null);
+  idCiutatSeleccionada = computed(() => this.ciutatSeleccionada()?.id ?? null);
 
+  private preguntaService = inject(Pregunta);
+  preguntaActual = signal<PreguntaDTO | null>(null);
+  idPreguntaActual = computed(() => this.preguntaActual()?.id ?? null);
+  situacions = signal<SituacioDTO[]>([]);
+  respostes = signal<RespostaDTO[]>([]);
+
+  ngOnInit(): void {
+    this.ciutatsService.getCiutats().subscribe((ciutats: CiutatDTO[]) => {
+      this.ciutats.set(ciutats);
+    });
+  }
+
+  actualitzarCiutat(ciutatOutput: CiutatDTO | undefined){
+    this.ciutatSeleccionada.set(ciutatOutput ?? null);
+    this.carregaSituacionsInicials();
+  }
+
+  carregaSituacionsInicials(){
+    if(this.idCiutatSeleccionada()){
+
+/*
+A DESENVOLUPAR:
+pensar com se sap la primera pregunta d'una ciutaT!!!
+si és final (o sigui, no pregunta sinó final d'itinerari, que no té respostes) marcar amb una resposta -1
+*/
+
+
+      //Carrega la primera pregunta
+      this.preguntaService.getPreguntesByCiutat(this.idCiutatSeleccionada()!).subscribe({
+        next: (pregunta) => this.preguntaActual.set(pregunta ?? null),
+        error: (error) => console.error('Error al obtener las preguntas:', error)
+      });
+      //Carrega les situacions de la primera pregunta
+
+    } 
+  }
+
+  
 }
--- src/app/Shared/Models/itinerari.dto.ts
+++ src/app/Shared/Models/itinerari.dto.ts
@@ -1,5 +1,19 @@
+import { SituacioDTO } from "../../Situacions/Model/situacio.dto";
+
 export interface ItinerariDTO {
-    id: number;
-    ciutat: number;
+    id?: number;
+    ciutat?: number;
     usuariaId?: number;
+}
+
+export interface ItinerariSeguitDTO{
+    itinerari: ItinerariDTO,
+    passos?: PasDTO[]
+}
+
+export interface PasDTO {
+    id?: number,
+    itinerariId?: number,
+    preguntaId: number,
+    respostaId: number,
 }
